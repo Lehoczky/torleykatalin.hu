96dcdaaa2cb66b3e12feb1028bb908c934f23e59
Lehoczky Zoltán <ifjlehoczkyzoltan@gmail.com>
2024-03-29 21:42:26 +0100

    fix link targets

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -3,10 +3,12 @@ export const EMAIL_ADDRESS = "info@torleykatalin.hu"
 export const SECTION_ID = {
   support: "tamogass",
   goals: "ugyek",
+  faq: "gyakori-kerdesek",
   contact: "kapcsolat",
 } as const
 
 export const EXTERNAL_LINK = {
+  donate: "https://actionnetwork.org/fundraising/tamogasd-a-kampanyomat-2",
   joinCampaign:
     "https://actionnetwork.org/forms/csatlakozz-a-torley-kampanyhoz-5/",
   facebook: "https://www.facebook.com/torley.kata/",
@@ -15,4 +17,6 @@ export const EXTERNAL_LINK = {
   youtube: "https://www.youtube.com/channel/UCuUyrk5t4nzx6htil1hXgrQ",
   newsletter:
     "https://actionnetwork.org/forms/csatlakozz-a-torley-kampanyhoz-3",
+  tanitanek: "https://www.tanitanek.info/",
+  mkkp: "https://ketfarkukutya.mkkp.party/",
 } as const
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -9,3 +9,16 @@ export function withBase(link: string): string {
   const linkPrefixed = import.meta.env.BASE_URL + "/" + link
   return linkPrefixed.replaceAll("//", "/")
 }
+
+export function anchorAttributes(attrs: Record<string, any>) {
+  const target: HTMLAnchorElement["target"] =
+    attrs.href && isExternalLink(attrs.href) && attrs.target == undefined
+      ? "_blank"
+      : attrs.target
+
+  return { ...attrs, target }
+}
+
+function isExternalLink(link: string): boolean {
+  return link.startsWith("http")
+}
